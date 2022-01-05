a491a43e593e9c77c5fd8dd1f24eff3dbe688ca9
midie-merovci <68757933+midie-merovci@users.noreply.github.com>
2022-01-05 21:38:20 +0100

    Finding wifi names only

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ data = subprocess.check_output(['netsh', 'wlan', 'show', 'profile']).decode('utf
 profiles = []
 for profile in data:
     print(profile)
+    if "All User Profile" in profile:
+        print(profile.split(":"))
+        profiles.append(profile.split(":")[1][1:-1])
+print(profiles)
