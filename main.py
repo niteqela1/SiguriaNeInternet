df61eab55f3b769fc15c8a1836769f76ed883128
Valdete <75278492+valdetesalihi@users.noreply.github.com>
2022-01-05 22:12:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ for profile in data:
         print(profile.split(":"))
         profiles.append(profile.split(":")[1][1:-1])
 print(profiles)
+for profile in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode('utf-8').split('\n')
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    try:
+        print ("{:<35}|  {:<}".format(profile, results[0]))
+    except IndexError:
+        print ("{:<35}|  {:<}".format(profile, ""))
